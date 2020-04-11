f39d5a66c01293a9e5d6f950fa684de5c52f75ee
rishu007 <rishulakhotiya@gmail.com>
2020-04-11 18:18:36 +0530

    string erase functions

---
+++ String/StringErase.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+void eraseAll(string &input)
+{
+    input.erase(); //erase all elements in a string
+}
+
+void eraseFromIndex(string &inputString, int index)
+{
+    inputString.erase(index); //remove characters starting at index to end;
+}
+
+void eraseFromIndexUptoLength(string &inputString, int index, int length)
+{
+    inputString.erase(index, length); //remove given length characters starting from index;
+}
+
+void eraseSingleCharacterAtGivenIndexUsingIterator(string &inputString, string::iterator itr, int index)
+{
+    advance(itr, index); //iterator at pointing at index
+    //or itr=itr+index
+    inputString.erase(itr); //remove single character pointed by the iterator;
+}
+
+void eraseFromOneIndexToAnotherIndex(string &inputString, int startingIndex, int endIndex)
+{
+
+    string::iterator start = next(inputString.begin(), startingIndex);
+    string::iterator end = next(inputString.begin(), endIndex);
+
+    inputString.erase(start, end);
+}
+int main()
+{
+
+    string test = "RemoveAllCharacters";
+    eraseFromOneIndexToAnotherIndex(test, 1, 5);
+    cout << test;
+    return 0;
+}
